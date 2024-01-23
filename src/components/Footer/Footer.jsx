0352a3af6f2350c81bbc2b59344d6bd73be8a9a5
Ana Dumont <dev.anais.dumont@gmail.com>
2024-01-23 16:24:57 +0100

    mise en page du footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,5 @@
 import "./Footer.css"
 
-// Import des composants
-import { Link } from "react-router-dom";
-
 // Création de la fonction composant du footer
 function Footer() {
 
@@ -11,10 +8,10 @@ function Footer() {
   
     return (
         <footer>
-            <p>Site créé et réalisé par 
-                <a href="https://alizaryana.com" target="_blank" rel="noopener noreferrer"> Alizaryana </a>
-            </p>
             <p>&copy; {currentYear} Café des Sciences. Tous droits réservés.</p>
+            <p>
+                <a href="https://alizaryana.com" target="_blank" rel="noopener noreferrer">Site créé et réalisé par Alizaryana </a>
+            </p>
             <p><a href="/Legal">Mentions légales</a></p>
             <p><a href="/Regulations">Statuts et règlement intérieur</a></p>
         </footer>
