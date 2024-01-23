6d8c75ae26eb15ca14b40411843d47c5ebc9085d
Ana Dumont <dev.anais.dumont@gmail.com>
2024-01-23 15:39:42 +0100

    Ajout fausses data members

--- src/data/DataMembers.js
+++ src/data/DataMembers.js
@@ -6,15 +6,16 @@ const DataMembers = [
     image:
       "https://ugc.production.linktr.ee/99693349-fc9a-4f09-a1dd-9a6bbd57d558_Capture-d-e-cran-2024-01-17-a--01.07.56-min.png?io=true&size=avatar-v1_0",
     tags: ["Biologie", "Neurosciences", "Sciences du vivant"],
+    shortdescription:
+      "Les Explorers est un collectif de vulgarisateurices scientifiques multidisciplinaire.",
+    description:
+      "Nous mettons l'exploration au service du partage des sciences à travers des formats innovants à l'interface entre la science, la culture (musique, cinéma, art, aventure, voyage, etc.) et les citoyen·nes. Nous avons réalisé le projet plusieurs fois nominé Cell Worlds, reconnu par la communauté scientifique internationale. Nous sommes membres du Café des Sciences, nous collaborons avec Jamy (Epicurieux), et nous avons été 2 fois lauréats de la bourse CNC Savoirs & Cultures.",
+
     links: {
       youtube: "https://www.youtube.com/channel/UC1QfxJ9W8aRRMyCuv7avc0g",
       instagram: "https://www.instagram.com/explorersfr",
       website: "https://www.linktr.ee/explorersfr",
     },
-    shortdescription:
-      "Les Explorers est un collectif de vulgarisateurices scientifiques multidisciplinaire.",
-    description:
-      "Nous mettons l'exploration au service du partage des sciences à travers des formats innovants à l'interface entre la science, la culture (musique, cinéma, art, aventure, voyage, etc.) et les citoyen·nes. Nous avons réalisé le projet plusieurs fois nominé Cell Worlds, reconnu par la communauté scientifique internationale. Nous sommes membres du Café des Sciences, nous collaborons avec Jamy (Epicurieux), et nous avons été 2 fois lauréats de la bourse CNC Savoirs & Cultures.",
 
     content: {
       one: {
@@ -42,20 +43,21 @@ const DataMembers = [
 
   {
     id: "d67ab8a7",
-    pseudo: "Latin heureux",
-    name: "Lorem Ipsum deus",
+    pseudo: "Sci-Explora",
+    name: " Aria Thompson, docteure en biologie moléculaire",
     image:
       "https://img.freepik.com/photos-gratuite/femme-travaillant-dans-laboratoire-microscope_144627-41604.jpg?t=st=1702902589~exp=1702903189~hmac=9647f144c88bfcd2d23b04cd8f639f49022ea85cc18a37f71891a47915927ae1",
-    tags: ["Physique", "Sciences"],
+    tags: ["Biologie moléculaire", "Génétique", "Biotechnologie"],
+    shortdescription:
+      "Explorez les merveilles de la science avec le Dr. Aria. Des concepts complexes expliqués simplement !",
+    description:
+      "Le Dr. Aria Thompson, docteur en biologie moléculaire, a consacré sa carrière à rendre la science accessible à tous. Avec une passion inébranlable pour la communication scientifique, elle combine une expertise académique approfondie avec un talent exceptionnel pour expliquer les sujets complexes de manière simple et captivante. Ses vidéos éducatives couvrent un large éventail de sujets, de la génétique évolutive aux dernières avancées en biotechnologie. Rejoignez-la pour un voyage enrichissant à travers les fondements fascinants de la vie.",
+
     links: {
       youtube: "https://www.youtube.com",
       mastodon: "https://www.x.com",
       instagram: "https://www.vinted.fr",
     },
-    shortdescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
 
     content: {
       one: {
@@ -83,21 +85,23 @@ const DataMembers = [
   },
 
   {
-    id: "f5462s",
-    name: "Lorem Ipsum sans pseudo",
+    id: "c67fds5fsab8a7",
+    pseudo: "QuantumQuill",
+    name: "Dr. Ethan Reynolds, docteur en Astrophysique",
     image:
-      "https://f.hellowork.com/obs-static-images/seo/ObsJob/sociologue.jpg",
-    tags: ["Sociologie", "Histoire", "Psychologie"],
+      "https://img.freepik.com/photos-premium/scientifique-planetaire-ia-generative_250469-11572.jpg?w=1060",
+    tags: ["Physique quantique", "Astrophysique", "Exploration spatiale"],
+    shortdescription:
+      "Plongez dans le monde fascinant de la physique quantique avec le Dr. Ethan.",
+    description:
+      "Le Dr. Ethan Reynolds, docteur en physique quantique et chercheur émérite, a consacré sa vie à dévoiler les mystères du cosmos. Son blog, QuantumQuill, est une vitrine intellectuelle où il partage des analyses profondes sur la mécanique quantique, l'astrophysique et l'exploration spatiale. Sa capacité à rendre les concepts complexes compréhensibles et passionnants en fait une référence incontournable pour les passionnés de science et les curieux de tous horizons.",
+
     links: {
       youtube: "https://www.youtube.com",
       instagram: "https://www.x.com",
       website: "https://www.vinted.fr",
       tip: "https://fr.tipeee.com/",
     },
-    shortdescription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
 
     content: {
       one: {
@@ -123,24 +127,25 @@ const DataMembers = [
       },
     },
   },
+
   {
-    id: "gfd564",
-    pseudo: "Le père Noël",
-    name: "n'existe pas",
+    id: "c67abvbv8a7",
+    pseudo: "ChemiSimplicity",
+    name: "Maya Patel",
     image:
-      "https://img.freepik.com/photos-gratuite/homme-costume-pere-noel-clignotant_329181-6661.jpg?w=900&t=st=1703086589~exp=1703087189~hmac=ffb93d5b9ca03dde5cf7162f85f0304c4b7c279a12f0e19ef51c77732984d89c",
-    tags: ["Imagination", "Rêve de gosse"],
-    links: {
-      instagram: "https://www.instagram.com/",
-      twitter: "https://www.x.com",
-      website: "https://www.vinted.fr",
-      tip: "https://www.kisskissbankbank.com/",
-      mastodon: "https://joinmastodon.org/fr",
-    },
+      "https://img.freepik.com/photos-premium/recherche-forestiere-scientifique-testent-echantillon-eau-pour-inspection-ecosysteme-environnement-science-environnementaliste-durable-professionnelle-effectuant-examen-empreinte-carbone_590464-216543.jpg?w=826",
+    tags: ["Chimie", "Éducation scientifique", "Expériences", "Molécules"],
     shortdescription:
-      "Vêtu d'un costume rouge et blanc, il est souvent représenté comme un homme jovial, barbu et dodu, portant un bonnet rouge caractéristique. Selon la tradition populaire, il vit au pôle Nord avec ses rennes et ses lutins. ",
+      "Découvrez la magie de la chimie avec Maya. Des expériences fascinantes à la portée de tous et toutes !",
     description:
-      "Le Père Noël, également connu sous le nom de Santa Claus, est une figure emblématique associée à la saison des fêtes. Vêtu d'un costume rouge et blanc, il est souvent représenté comme un homme jovial, barbu et dodu, portant un bonnet rouge caractéristique. Selon la tradition populaire, il vit au pôle Nord avec ses rennes et ses lutins. Chaque année, la nuit du 24 décembre, il distribue des cadeaux aux enfants sages du monde entier en utilisant son traîneau magique tiré par des rennes volants. Sa générosité, sa bienveillance et son esprit de partage en font une figure aimée et attendue par les familles, symbolisant la magie et la joie de la période de Noël.",
+      "Maya Patel, titulaire d'un doctorat en chimie et pionnière dans le domaine de la vulgarisation scientifique, émerveille le public avec sa chaîne YouTube, ChemiSimplicity. Elle a débuté sa carrière en tant que chercheuse dans le domaine de la chimie organique avant de se consacrer pleinement à la diffusion de connaissances. Ses vidéos allient humour et pédagogie pour rendre la chimie accessible à tous, des débutants aux passionnés chevronnés.",
+
+    links: {
+      youtube: "https://www.youtube.com",
+      instagram: "https://www.x.com",
+      website: "https://www.vinted.fr",
+      tip: "https://fr.tipeee.com/",
+    },
 
     content: {
       one: {
@@ -166,23 +171,69 @@ const DataMembers = [
       },
     },
   },
+
   {
-    id: "fgdg95f61",
-    pseudo: "La mère Noël",
-    name: "",
+    id: "sf56s4",
+    pseudo: "SocioSight",
+    name: "Vanessa Rodriguez, sociologue",
     image:
-      "https://cdn-tam.ouest-france.fr/media/cache/carousel/assets/carousel/92e7eeb20d2669f49a984f5c1232ac1da6493ad1.png",
-    tags: ["Imagination", "Rêve de gosse", "pouet"],
-    links: {
-      instagram: "https://www.instagram.com/",
-      twitter: "https://www.x.com",
-      website: "https://www.vinted.fr",
-      mastodon: "https://joinmastodon.org/fr",
-    },
+      "https://img.freepik.com/photos-gratuite/techniciens-essayant-atteindre-objectifs-ambitieux-matiere-developpement-durable_23-2150950222.jpg?w=900&t=st=1706020118~exp=1706020718~hmac=69979d80c6608a7857ba20142a68e4b84eb0ddc55596fbbfb0efc2e627c14751",
+    tags: ["Sociologie", "Culture", "Identité"],
     shortdescription:
-      "Souvent représentée comme une femme aimante et bienveillante, elle est décrite comme une personne attentionnée, prenant soin des lutins et aidant le Père Noël dans la préparation des cadeaux pour les enfants du monde entier.",
+      "Explorez les dynamiques sociales avec le Dr. Vanessa. Décryptage de la société et compréhension des relations humaines !",
     description:
-      "La Mère Noël est une figure moins connue mais tout aussi importante que son époux, le Père Noël. Souvent représentée comme une femme aimante et bienveillante, elle partage l'esprit de Noël avec son mari depuis le pôle Nord. Son rôle varie selon les cultures et les histoires, mais généralement, elle est décrite comme une personne attentionnée, prenant soin des lutins et aidant le Père Noël dans la préparation des cadeaux pour les enfants du monde entier. Son apparence peut différer selon les récits, mais elle est souvent représentée avec des vêtements chaleureux et colorés, reflétant son côté maternel et sa générosité pendant la période des fêtes. Bien que moins médiatisée que le Père Noël, la Mère Noël incarne également la magie et l'esprit de Noël, apportant chaleur et réconfort aux foyers du monde entier.",
+      " Vanessa Rodriguez se passionne pour l'analyse des dynamiques sociales contemporaines. À travers sa plateforme, SocioSight, elle offre des perspectives éclairantes sur des sujets tels que la culture, l'identité et les enjeux sociaux. Son approche holistique de la sociologie permet de comprendre les relations humaines dans un contexte global. Rejoignez SocioSight pour des discussions stimulantes et des analyses approfondies sur les forces qui façonnent nos sociétés.",
+
+    links: {
+      youtube: "https://www.youtube.com",
+      instagram: "https://www.x.com",
+      website: "https://www.vinted.fr",
+      tip: "https://fr.tipeee.com/",
+    },
+
+    content: {
+      one: {
+        image:
+          "https://thumbor.comeup.com/unsafe/fit-in/630x400/filters:quality(90):no_upscale()/uploads/media/picture/2022-01-10/miniature-youtube-5-euros-61db6e98249f3.png",
+        link: "https://www.youtube.com",
+        title: "Video Youtube",
+        description: "Voici la vidéo youtube de Lorem Ipsum",
+      },
+      two: {
+        image:
+          "https://static.lpnt.fr/images/2021/10/06/22290110lpw-22290331-article-twitch-donnees-piratage-jpg_8284871_1250x625.jpg",
+        link: "https://www.twitch.tv",
+        title: "Video Twitch",
+        description: "Voici la vidéo Twitch de Lorem Ipsum",
+      },
+      three: {
+        image:
+          "https://uploads-ssl.webflow.com/5d26423fb77ae083e4ddd2e3/5d26423fb77ae01ee3ddd509_blogging.jpg",
+        link: "https://www.vinted.fr",
+        title: "Lien blog",
+        description: "Voici le lien blog de Lorem Ipsum",
+      },
+    },
+  },
+
+  {
+    id: "i67ab8a7",
+    pseudo: "",
+    name: "Dr. Thomas Walker, historien",
+    image:
+      "https://img.freepik.com/photos-gratuite/beau-jeune-ecrivain-assis-table-ecrit-quelque-chose-dans-son-carnet-croquis-maison_155003-17075.jpg?w=740&t=st=1706020235~exp=1706020835~hmac=cbf65ee7a1b60a3a8fe7c8df433cbb21c373816c0538d86a35bd7a51c3cb6ef0",
+    tags: ["Histoire", "Patrimoine", "Archéologie"],
+    shortdescription:
+      "Plongez dans le passé avec Thomas. Découvrez l'histoire à travers des récits captivants et des fouilles archéologiques !",
+    description:
+      "Dr. Thomas Walker, historien érudit, vous invite à explorer les méandres du passé sur HistoryHarbor. Spécialisé dans l'étude du patrimoine et des événements historiques, le Dr. Walker partage des récits captivants et des découvertes issues de fouilles archéologiques. Sa passion pour l'histoire prend vie à travers des anecdotes riches et des analyses approfondies qui éclairent les périodes cruciales de notre passé. Rejoignez HistoryHarbor pour un voyage temporel fascinant à travers les époques.",
+
+    links: {
+      youtube: "https://www.youtube.com",
+      instagram: "https://www.x.com",
+      website: "https://www.vinted.fr",
+      tip: "https://fr.tipeee.com/",
+    },
 
     content: {
       one: {
